92bbf1597359da53ffa315ea91868c0acb623862
adriannaluz <adriannaluz@gmail.com>
2024-03-21 15:19:01 +0100

    Correcting displayed message with just content

--- chat_bot/visual_interface.py
+++ chat_bot/visual_interface.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 from funcs import sendPrompt
 
-st.write(st.session_state.keys)
-
+# Title of my app
 st.title("Chat with Ollama")
+
+# Defining messages
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question!"}
     ]
 
+# Creating chat input box
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -22,6 +24,6 @@ if st.session_state.messages[-1]['role'] != "assistant":
         with st.spinner("Thinking..."):
             response = sendPrompt(prompt)
             print(response)
-            st.write(response)
+            st.write(response.content)  # Display only the content of the response
             message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message)

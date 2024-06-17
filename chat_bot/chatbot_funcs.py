52a8bc28a6cb98e7531c98f54efbe73c7a1f6136
adriannaluz <adriannaluz@gmail.com>
2024-06-17 15:38:16 +0200

    Correcting typo

--- chat_bot/chatbot_funcs.py
+++ chat_bot/chatbot_funcs.py
@@ -80,7 +80,7 @@ def run_llm_model():
     # Prompt template
     template = (
         "Use the following pieces of context to answer the question at"
-        " the end. If the question id in Spanish reply in Spanish, "
+        " the end. If the question is in Spanish reply in Spanish, "
         "if not reply in English. If you don’t know the answer, just "
         "say that you don’t know, don’t try to make up an answer. Use "
         "three sentences maximum and keep the answer as concise as "
